39f3a85688d5faf4f4491b43a8df375f9955c45d
lehrmanaidins <lehrmanaidin+github.com@gmail.com>
2024-04-23 20:22:12 -0400

    Correctly Implemented Multi-Threading

--- network_training.py
+++ network_training.py
@@ -20,32 +20,29 @@ import threading
 image_width = 20
 image_height = 20
 
-weights = np.zeros(image_width * image_height)
+weights: list[float] = list(np.zeros(image_width * image_height))
 
-def train(answers_file_path: str, training_data):
+average_accuracy: float = 0.0
 
-    global weights
+def train(answers_list: list[str], training_data):
+    
+    guessed_correctly: int = 0
 
-    with open(answers_file_path, 'r', encoding='utf-8') as answers_file:
+    for image, desired_output_str in zip(training_data, answers_list):
+        
+        global weights # Gets updated weight values
 
-        guessed_correctly: int = 0
+        raw_neural_network_output = np.dot(image, weights)
+        cleaned_neural_network_output = min(1.0, max(0.0, raw_neural_network_output))
 
-        for image, desired_output_str in zip(training_data, answers_file):
+        desired_output = 1.0 if desired_output_str.strip() == 'circle' else 0.0
 
-            raw_neural_network_output = np.dot(image, weights)
-            cleaned_neural_network_output = min(1.0, max(0.0, raw_neural_network_output))
+        error = desired_output - cleaned_neural_network_output 
 
-            desired_output = 1.0 if desired_output_str.strip() == 'circle' else 0.0
+        if (desired_output == cleaned_neural_network_output):
+            guessed_correctly += 1
 
-            error = desired_output - cleaned_neural_network_output 
-
-            if (desired_output == cleaned_neural_network_output):
-                guessed_correctly += 1
-
-            weights += np.multiply(image, error)
-
-    with open('./weights.csv', 'w', encoding='utf-8') as weight_file:
-        weight_file.write(str(list(weights))[1: -1])
+        weights += np.multiply(image, error)
 
     return guessed_correctly / len(training_data)
 
@@ -55,36 +52,68 @@ def train_cycles(answers_file_path, images, max_cycles: int) -> None:
     # It is necessary to cycle through and train with the same samples a couple of times.
     for i in range(max_cycles):
         percent_guess_correctly: float = train(answers_file_path, images)
-
-        print(f'\r\tTraining ... (#{i + 1}), Max Cycles: {max_cycles}. Percent Accuracy: {percent_guess_correctly * 100:.2f}%', end='')
-
-        # Passed most tests
-        if (percent_guess_correctly > 0.9999):
-            break
         
-    print('\nDone Training.')
+        global average_accuracy
+        average_accuracy = (percent_guess_correctly + average_accuracy) / 2
+
+        if (i % 50 == 0):
+            print(f'\r\tTraining ... (#{i + 1:0>4}), Max Cycles: {max_cycles}. Percent Accuracy: {average_accuracy * 99.99:>6.2f}%', end='')
     
 
 def main() -> None:
+    
+    global weights
+    
+    # Get weight values stored in 'weights.csv' file
+    with open('./weights.csv', 'r', encoding='utf-8') as weight_file:
+        weights = [float(value.strip()) for value in weight_file.readline().strip().split(', ')]
+    
     num_samples = 10_000
 
     answers_file_path = os.path.join('.', 'training_answers.csv')
     
     training_data = generate_training_data(answers_file_path, n=num_samples, image_width=image_width, image_height=image_height)
     flattened_training_data = [list(np.reshape(image, image_width * image_height)) for image in training_data]
+    
+    answers_list: list[str] = []
+    with open(answers_file_path, 'r', encoding='utf-8') as answers_file:
+        for line in answers_file:
+            answers_list.append(line)
 
     threads = []
 
-    for _ in range(10):
-        thread = threading.Thread(target=train_cycles, args=(answers_file_path, flattened_training_data, 50))
-        thread.start()
+    num_threads: int = 100
+    max_num_cycles: int = 1_000
+    
+    for i in range(num_threads):
+        thread = threading.Thread(
+            target=train_cycles,
+            args=(
+                answers_list[i * int(num_samples / num_threads): (i + 1) * int(num_samples / num_threads)],
+                flattened_training_data[i * int(num_samples / num_threads): (i + 1) * int(num_samples / num_threads)],
+                max_num_cycles
+            )
+        )
         threads.append(thread)
+        
+    for thread in threads:
+        thread.start()
 
     for thread in threads:
         thread.join()
+        
+    with open('./weights.csv', 'w', encoding='utf-8') as weight_file:
+        weight_file.write(str(list(weights))[1:-1])
  
-    print("Done!")
+    print("\nDone Training!")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+        
+    except (InterruptedError): # Saves weight values if InterruptedError exception occurs
+        pass
+    finally:
+        with open('./weights.csv', 'w', encoding='utf-8') as weight_file:
+            weight_file.write(str(list(weights))[1:-1])
